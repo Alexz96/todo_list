ffd8e12693df636278004202b27bbf14e7b47418
Alexsander <alexsander1996@yahoo.com.br>
2020-05-03 20:02:33 -0300

    Added function to organize the To Do's that are done to the bottom and also solved a little bug caused by the SnackBars

--- lib/main.dart
+++ lib/main.dart
@@ -45,6 +45,23 @@ class _HomeState extends State<Home> {
     });
   }
 
+  Future<Null> _refresh() async {
+    await Future.delayed(Duration(seconds: 1));
+
+    setState(() {
+      // Ordenation of the list using Dart language with anonymous function
+      _toDoList.sort((a, b) {
+        if(a["ok"] && !b["ok"]) return 1;
+        else if(!a["ok"] && b["ok"]) return -1;
+        else return 0;
+      });
+
+      _saveData();
+    });
+
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -78,10 +95,12 @@ class _HomeState extends State<Home> {
             ),
           ),
           Expanded(
-            child: ListView.builder(
-              padding: EdgeInsets.only(top: 10.0),
-              itemCount: _toDoList.length,
-              itemBuilder: buildItem),
+            child: RefreshIndicator(onRefresh: _refresh,
+              child: ListView.builder(
+                  padding: EdgeInsets.only(top: 10.0),
+                  itemCount: _toDoList.length,
+                  itemBuilder: buildItem),
+            ),
           )
         ],
       ),
@@ -131,6 +150,7 @@ class _HomeState extends State<Home> {
             duration: Duration(seconds: 2),
           );
 
+          Scaffold.of(context).removeCurrentSnackBar(); // Code to prevent Stacking of SnackBars and a bug
           Scaffold.of(context).showSnackBar(snack);
         });
       },
